e2f2c4e19122c96b68b38b8253ddb683360e97bc
George Tekelis <tekelisg@gmail.com>
2016-03-11 16:58:51 +0200

    added summary

--- Text_Udemy_Ex2/Program.cs
+++ Text_Udemy_Ex2/Program.cs
@@ -8,8 +8,16 @@ namespace Text_Udemy_Ex2
 {
     class Program
     {
+        /// <summary>
+        /// Write a program and ask the user to enter a few numbers separated by a hyphen. 
+        /// If the user simply presses Enter, without supplying an input, exit immediately; 
+        /// otherwise, check to see if there are duplicates. 
+        /// If so, display "Duplicate" on the console.
+        /// </summary>
+
         static void Main(string[] args)
         {
+
         }
     }
 }
