718c917c287d41e7ac0b7bae424255fb82acc58c
George Tekelis <tekelisg@gmail.com>
2016-03-12 23:57:28 +0200

    new projects

---
+++ Text_Udemy_Ex3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Udemy_Ex3
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        
+        static void Main(string[] args)
+        {
+        }
+    }
+}
