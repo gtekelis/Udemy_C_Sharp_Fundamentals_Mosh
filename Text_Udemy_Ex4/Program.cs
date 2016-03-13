f84bfaf202e09f9e948b83720493d6ee83ccc407
George Tekelis <tekelisg@gmail.com>
2016-03-13 19:57:00 +0200

    text ex4 ok

--- Text_Udemy_Ex4/Program.cs
+++ Text_Udemy_Ex4/Program.cs
@@ -8,8 +8,37 @@ namespace Text_Udemy_Ex4
 {
     class Program
     {
+        /// <summary>
+        /// Write a program and ask the user to enter a few words separated by a space. 
+        /// Use the words to create a variable name with PascalCase. 
+        /// For example, if the user types: "number of students", display "NumberOfStudents". 
+        /// Make sure that the program is not dependent on the input. 
+        /// So, if the user types "NUMBER OF STUDENTS", the program should still display "NumberOfStudents".
+        /// </summary>
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a few words separated by spaces...");
+            var input = Console.ReadLine();
+
+            var arr = input.Split(' ');
+            var strinBldr = new StringBuilder();
+            string temp;
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                temp = arr[i].ToLower();
+                for (int j = 0; j < arr[i].Length; j++)
+                {                    
+                    if (j==0)
+                    {
+                        strinBldr.Append(temp[j].ToString().ToUpper());
+                        continue;
+                    }
+                    strinBldr.Append(temp[j]);                   
+                }                               
+            }
+            Console.WriteLine(strinBldr);
         }
     }
 }
