1bebfe7685b83b5c93f25b0bec30012623dadc33
George Tekelis <tekelisg@gmail.com>
2016-03-13 19:00:41 +0200

    text ex3 ok

--- Text_Udemy_Ex3/Program.cs
+++ Text_Udemy_Ex3/Program.cs
@@ -9,11 +9,40 @@ namespace Text_Udemy_Ex3
     class Program
     {
         /// <summary>
-        /// 
+        /// Write a program and ask the user to enter a time value in the 24-hour time format (e.g. 19:00).
+        /// A valid time should be between 00:00 and 23:59.
+        /// If the time is valid, display "Ok"; otherwise, display "Invalid Time".
+        /// If the user doesn't provide any values, consider it as invalid time.
         /// </summary>
-        
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter time...");
+                    var input = Console.ReadLine();
+                    var parsedTime = DateTime.Parse(input);
+
+                    var minTime = DateTime.Parse("00:00");
+                    var maxTime = DateTime.Parse("23:59");
+
+
+                    if (parsedTime >= minTime && parsedTime <= maxTime)
+                    {
+                        Console.WriteLine("time is ok");
+                        break;
+                    }
+                    Console.WriteLine("invalid time");
+                    continue;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("invalid time");
+                    continue;
+                }
+            }
         }
     }
 }
